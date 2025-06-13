4f417f9a08c7e9f1284d22bb07f5ebdaffcb458c
yabalap <gelpalabay05@gmail.com>
2025-06-13 16:03:06 +0800

    ss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
