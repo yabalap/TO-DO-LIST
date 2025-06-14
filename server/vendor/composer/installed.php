8dd8b1329357314e98e99facc42f227dc2340194
yabalap <gelpalabay05@gmail.com>
2025-06-14 15:43:45 +0800

    installing composer server side

---
+++ server/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'angelopalabay/to-do-list',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '429bb4a73344daa3e8b8f8fe41d0f10aa61a9e28',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'angelopalabay/to-do-list' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '429bb4a73344daa3e8b8f8fe41d0f10aa61a9e28',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
